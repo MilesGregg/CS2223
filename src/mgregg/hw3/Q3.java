29e1b692891f39612c7fc355484027e0c28dedff
Miles <milesgregg3@gmail.com>
2021-04-25 03:40:05 -0600

    problem 3 work

--- src/mgregg/hw3/Q3.java
+++ src/mgregg/hw3/Q3.java
@@ -2,13 +2,27 @@ package mgregg.hw3;
 
 import algs.hw3.ShakespearePlay;
 
-import java.util.Iterator;
-
 public class Q3 {
 
+    private static String mostCommon;
 
-    public static void main(String[] args) {
+    private static void findMostCommonInAll() {
+        BST bst = new BST();
+        for (int i = 1; i <= 38; i++) {
+            ShakespearePlay shakespearePlay = new ShakespearePlay(i);
+            for (String s : shakespearePlay) {
+                if (bst.get(s) != null) {
+                    bst.put(s, bst.get(s) + 1);
+                } else {
+                    bst.put(s, 1);
+                }
+            }
+        }
+        mostCommon = bst.mostFrequent();
+        System.out.println("The most common word in all 38 plays is: " + mostCommon);
+    }
 
+    private static void part2() {
         for (int i = 1; i <= 38; i++) {
             ShakespearePlay shakespearePlay = new ShakespearePlay(i);
             BST bst = new BST();
@@ -22,12 +36,62 @@ public class Q3 {
             StringBuilder outputString = new StringBuilder();
             for (int j = 0; j < 5; j++) {
                 String mostFrequent = bst.mostFrequent();
-                outputString.append(mostFrequent).append("\t\t");
+                outputString.append(String.format("%-10s\t", mostFrequent));
                 bst.delete(mostFrequent);
             }
-            outputString.append(shakespearePlay.getTitle());
+            outputString.append(String.format("%-10s\t", shakespearePlay.getTitle()));
             System.out.println(outputString);
         }
+    }
+
+    private static void part3() {
+        for (int i = 1; i <= 38; i++) {
+            ShakespearePlay shakespearePlay = new ShakespearePlay(i);
+            BST bst = new BST();
+            for (String s : shakespearePlay) {
+                if (bst.get(s) != null) {
+                    bst.put(s, bst.get(s) + 1);
+                } else {
+                    bst.put(s, 1);
+                }
+            }
+            StringBuilder outputString = new StringBuilder();
+            for (int j = 0; j < 5; j++) {
+                String mostFrequent = bst.mostFrequent();
+                outputString.append(String.format("%s\t", mostFrequent));
+                bst.delete(mostFrequent);
+            }
+            outputString.append(String.format("%15s", shakespearePlay.getTitle()));
+            System.out.println(outputString);
+        }
+    }
+
+    private static void findLongestNonHyphenated() {
+        String longestWord = "";
+        for (int i = 1; i <= 38; i++) {
+            ShakespearePlay shakespearePlay = new ShakespearePlay(i);
+            for (String s : shakespearePlay) {
+                if (s.length() > longestWord.length()) {
+                    for (int j = 0; j < s.length(); j++) {
+                        if (s.charAt(j) == '-') {
+                            break;
+                        } else if (j == s.length() - 1) {
+                            longestWord = s;
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println("Longest Word: " + longestWord);
+    }
+
+    public static void main(String[] args) {
+        findLongestNonHyphenated();
+        //findMostCommonInAll();
+        //part2();
+
+
 
         /*for (int i = 1; i <= 38; i++) {
             ShakespearePlay shakespearePlay = new ShakespearePlay(i);
@@ -48,15 +112,6 @@ public class Q3 {
             outputString.append(shakespearePlay.getTitle());
             System.out.println(outputString);
         }*/
-
-
-
-
-
-
-
-
-
 
 
         /*while (iterator.hasNext()) {
