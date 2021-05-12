130e54b3975ba727ebf3a69604c77cf600b45544
Miles <milesgregg3@gmail.com>
2021-05-12 16:00:40 -0600

    fixes

--- src/mgregg/BSTTesting.java
+++ src/mgregg/BSTTesting.java
@@ -23,7 +23,25 @@ public class BSTTesting {
             return deleteMax(node.right);
         }
 
+        void insert(int key) {
+            root = insertRec(root, key);
+        }
 
+        Node insertRec(Node root, int key) {
+            if (root == null)
+            {
+                root = new Node();
+                root.key = key;
+                return root;
+            }
+
+            if (key < root.key)
+                root.left = insertRec(root.left, key);
+            else if (key > root.key)
+                root.right = insertRec(root.right, key);
+
+            return root;
+        }
 
         public int countBetween(int lo, int hi) {
             return countBetween(lo, hi, root);
@@ -34,7 +52,7 @@ public class BSTTesting {
                 return 0;
             }
 
-            if (node.key <= lo && node.key >= hi) {
+            if (node.key <= hi && node.key >= lo) {
                 return 1 + countBetween(lo, hi, node.left) + countBetween(lo, hi, node.right);
             } else if (node.key < lo) {
                 return countBetween(lo, hi, node.right);
@@ -44,8 +62,34 @@ public class BSTTesting {
         }
     }
 
+//    public void allSources() {
+//        dfsMain(3);                        // O(V + E)
+//        for (int v = 0; v < V; v++) {      // O(V)
+//            if (marked[v] == true) {
+//                System.out.println(v);
+//            }
+//        }
+//    }
+
     public static void main(String[] args) {
-        BST bst = new BST();
+        BST tree = new BST();
+        /* Let us create following BST
+              50
+           /     \
+          30      70
+         /  \    /  \
+       20   40  60   80 */
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+       // tree.insert(80);
+        System.out.println("Size: " + tree.countBetween(40, 70));
+        tree.deleteMax();
+
+        /*BST bst = new BST();
         BST.Node node1 = new BST.Node();
         node1.key = 1;
         BST.Node node2 = new BST.Node();
@@ -60,7 +104,7 @@ public class BSTTesting {
         bst.root.right.left = node3;
         bst.root.right.right = node4;
 
-        bst.deleteMax();
+        bst.deleteMax();*/
     }
 
 }
